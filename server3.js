f3efe8ac5c3ff180edb247e4670fdf45872bb466
robin <476296987@qq.com>
2018-05-08 17:32:55 +0800

    auto get ip

--- server3.js
+++ server3.js
@@ -1,13 +1,25 @@
 var http = require('http');
-
+function getIPAdress(){  
+    var interfaces = require('os').networkInterfaces();  
+    for(var devName in interfaces){  
+          var iface = interfaces[devName];  
+          for(var i=0;i<iface.length;i++){  
+               var alias = iface[i];  
+               if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){  
+                     return alias.address;  
+               }  
+          }  
+    }  
+}
+var ip = getIPAdress();
 var server = http.createServer(function(req, res) {
     console.log('request for: ' + req.url + '::3002');
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
     res.end('Sample Message. Served by port 3002')
-}).listen(3002, '127.0.0.1');
+}).listen(3002, ip);
 
 server.once('listening', function() {
-    console.log('Server running at 127.0.0.1::3002')
+    console.log('Server running at '+ ip +'::3002')
 })
