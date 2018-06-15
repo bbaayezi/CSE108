fcfe3d02c834196881c4283c38573691ca2ce9fe
robin <476296987@qq.com>
2018-06-15 15:03:23 +0800

    update folder

--- servers/server1.js
+++ servers/server1.js
@@ -17,8 +17,9 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
+    console.log(req.headers);
     res.end('Sample Message. Served by port 3000')
-}).listen(3000, ip);
+}).listen(3000, '127.0.0.1');
 
 server.once('listening', function() {
     console.log('Server running at ' + ip + '::3000')
--- test.js
+++ test.js
@@ -1,8 +1,23 @@
 var http = require('http');
 var request = require('request');
-
-for (var i = 0; i < 50; i++) {
-    request('http://192.168.1.11', function(err, res, body) {
-        if (!err && res.statusCode == 200) console.log(body)
+let times = [];
+let time = 0;
+let date1 = new Date().getTime();
+// let date2;
+for (let i = 0; i < 2000; i++) {
+    let key = i;
+    // console.log(key)
+    // console.log('sending request...')
+    // console.time(key);
+    request('http://192.168.1.13', function(err, res, body) {
+        let date2 = new Date().getTime();
+        time = date2 - date1;
+        times.push(time);
+        console.log(time);
+        console.log('Average: '+times.reduce((l, c) => l + c) / times.length);
     })
+    
 }
+
+// console.log(date1);
+// console.log('last time: ' + time)
